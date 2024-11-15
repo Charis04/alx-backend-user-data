29fc5f9be0242de7395f125d79b6787cf0684e47
charis <charisadu@gmail.com>
2024-11-15 22:44:36 +0100

    Basic - Allow password with ":"

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -64,9 +64,9 @@ class BasicAuth(Auth):
         if ":" not in decoded_base64_authorization_header:
             return (None, None)
 
-        credentials = decoded_base64_authorization_header.split(":")
+        credentials = decoded_base64_authorization_header.partition()(":")
 
-        return (credentials[0], credentials[1])
+        return (credentials[0], credentials[2])
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
