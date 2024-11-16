e87689449630ce947cf1cedc3a1e8c63f8bc75fb
charis <charisadu@gmail.com>
2024-11-16 17:20:32 +0100

    fix indentation

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -42,7 +42,7 @@ def pre_request() -> str:
     if not auth.require_auth(request.path, excluded_paths):
         return
     if (not auth.authorization_header(request)) and (
-        not auth.session_cookie(request)):
+            not auth.session_cookie(request)):
         abort(401)
 
     request.current_user = auth.current_user(request)
